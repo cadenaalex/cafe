bbedb57af66c8866522c0a7ae1b3e31eb4d7e514
Alex Cadena <cadenaalex944@gmail.com>
2024-04-29 12:14:20 -0500

    CAFE

---
+++ js/efectos.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    //Efecto menu
+    $('.menu a').each(function (index, elemento) {
+        $(this).css({
+            'top': '-200px'
+        });
+        $(this).animate({
+            top: '0'
+        }, 2000 + (index * 500))
+    })
+    if ($(window).width() > 800) {
+        $('header .texto').css({
+            opacity: 0,
+            marginTop: 0
+        });
+        $('header .texto').animate({
+            opacity:1,
+            marginTop: '-52px'
+        }, 1500)
+    }
+    //Scroll Elementos menu
+    var acercaDe = $('#acerca-de').offset().top,
+    menu =$('#platillos').offset().top,
+    galeria =$('#galeria').offset().top,
+    ubicacin=$('#ubicacion').offset().top;
+
+    $('btn-acerca-de').on('click',function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: acercaDe -100
+        },500)
+    })
+})
